13124b3deb31bf7180e77246009353b05dfdac0b
gtg489w <gtg489w@gmail.com>
2015-07-16 18:23:50 -0400

    Got plugin xml file to properly copy over the framework files

--- www/OpenEars.js
+++ www/OpenEars.js
@@ -1,4 +1,13 @@
-(function() {
+/*
+
+Open Ears Cordova plugin
+
+http://www.torchproducts.com
+http://cordova.apache.org
+
+*/
+
+cordova.define("com.torchproducts.cordova.openears.OpenEars", function(require, exports, module) { (function() {
 	var OpenEars, cordova, exec;
 
 	cordova = require("cordova");
@@ -19,20 +28,22 @@
 			return trigger("openEarsError", args);
 		};
 
-		OpenEars.prototype.startAudioSession = function() {
+		OpenEars.prototype.initialize = function() {
 			var success;
 			success = function(args) {
-				return trigger("startAudioSession", args);
+				console.log('got success', args);
+				return trigger("initialize", args);
 			};
-			return exec(success, genericHandleError, "OpenEars", "startAudioSession", []);
+			console.log('returning the exec');
+			return exec(success, genericHandleError, "OpenEars", "startListening", []);
 		};
 
-		OpenEars.prototype.generateLanguageModel = function(languageName, languageCSV) {
+		OpenEars.prototype.startListening = function() {
 			var success;
 			success = function(args) {
-				return trigger("generateLanguageModel", args);
+				return trigger("startListening", args);
 			};
-			return exec(success, genericHandleError, "OpenEars", "generateLanguageModel", [languageName, languageCSV.toUpperCase()]);
+			return exec(success, genericHandleError, "OpenEars", "startListening", []);
 		};
 
 		OpenEars.prototype.stopListening = function() {
@@ -43,124 +54,12 @@
 			return exec(success, genericHandleError, "OpenEars", "stopListening", []);
 		};
 
-		OpenEars.prototype.resumeListening = function(options) {
-			var success;
-			success = function(args) {
-				return trigger("resumeListening", args);
-			};
-			return exec(success, genericHandleError, "OpenEars", "resumeListening", []);
-		};
-
-		OpenEars.prototype.suspendRecognition = function() {
-			var success;
-			success = function(args) {
-				return trigger("suspendRecognition", args);
-			};
-			return exec(success, genericHandleError, "OpenEars", "suspendRecognition", []);
-		};
-
-		OpenEars.prototype.resumeRecognition = function() {
-			var success;
-			success = function(args) {
-				return trigger("resumeRecognition", args);
-			};
-			return exec(success, genericHandleError, "OpenEars", "resumeRecognition", []);
-		};
-
-		OpenEars.prototype.startListeningWithLanguageModelAtPath = function(languagemodel, dictionary, options) {
-			var success;
-			if (options == null) {
-				options = null;
-			}
-			success = function(args) {
-				return trigger("startListeningWithLanguageModelAtPath", args);
-			};
-			return exec(success, genericHandleError, "OpenEars", "startListeningWithLanguageModelAtPath", [languagemodel, dictionary, options]);
-		};
-
-		OpenEars.prototype.changeLanguageModelToFile = function(languagemodel, dictionary, options) {
-			var success;
-			if (options == null) {
-				options = null;
-			}
-			success = function(args) {
-				return trigger("changeLanguageModelToFile", args);
-			};
-			return exec(success, genericHandleError, "OpenEars", "changeLanguageModelToFile", [languagemodel, dictionary, options]);
-		};
-
-		OpenEars.prototype.say = function(phrase) {
-			var success;
-			success = function(args) {
-				return trigger("say", args);
-			};
-			return exec(success, genericHandleError, "OpenEars", "say", [phrase]);
-		};
-
 		OpenEars.prototype.events = {
 			startedListening: function() {
 				return trigger("startedListening");
 			},
 			stoppedListening: function() {
 				return trigger("stoppedListening");
-			},
-			detectedSpeech: function() {
-				return trigger("detectedSpeech");
-			},
-			finishedDetectingSpeech: function() {
-				return trigger("finishedDetectingSpeech");
-			},
-			suspendedRecognition: function() {
-				return trigger("suspendedRecognition");
-			},
-			resumedRecognition: function() {
-				return trigger("resumedRecognition");
-			},
-			startedCalibration: function() {
-				return trigger("startedCalibration");
-			},
-			finishedCalibration: function() {
-				return trigger("finishedCalibration");
-			},
-			continuousSetupDidFail: function() {
-				return trigger("continuousSetupDidFail");
-			},
-			testRecognitionCompleted: function() {
-				return trigger("testRecognitionCompleted");
-			},
-			receivedHypothesis: function(hypothesis, recognitionScore, utteranceID) {
-				return trigger("receivedHypothesis", {
-					hypothesis: hypothesis,
-					recognitionScore: recognitionScore,
-					utteranceID: utteranceID
-				});
-			},
-			changedLanguageModelToFile: function(modelPath, dictionaryPath) {
-				return trigger("changedLanguageModelToFile", {
-					modelPath: modelPath,
-					dictionaryPath: dictionaryPath
-				});
-			},
-			startedSpeaking: function() {
-				return trigger("startedSpeaking");
-			},
-			finishedSpeaking: function() {
-				return trigger("finishedSpeaking");
-			},
-			audioSessionInterruptionDidBegin: function() {
-				return trigger("audioSessionInterruptionDidBegin");
-			},
-			audioSessionInterruptionDidEnd: function() {
-				return trigger("audioSessionInterruptionDidEnd");
-			},
-			audioInputDidBecomeUnavailable: function() {
-				return trigger("audioInputDidBecomeUnavailable");
-			},
-			audioInputDidBecomeAvailable: function() {
-				return trigger("audioInputDidBecomeAvailable");
-			},
-			audioRouteDidChangeToRoute: function() {
-				return trigger("audioRouteDidChangeToRoute");
 			}
 		};
 
@@ -173,3 +72,4 @@
 	module.exports = OpenEars;
 
 }).call(this);
+});
